f0b85b48d806b4aeec857ea60f3fa88e12077853
Artem Averin <artem-averin-91@mail.ru>
2024-01-25 18:40:36 +0300

    4. Многопоточность в базе данных [#504860]

---
+++ src/main/java/ru/job4j/dreamjob/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.User;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface UserRepository {
+
+    Optional<User> save(User user);
+
+    Optional<User> findByEmailAndPassword(String email, String password);
+
+    public Collection<User> findAll();
+}
---
+++ src/test/java/ru/job4j/dreamjob/repository/Sql2oUserRepositoryTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.dreamjob.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+import ru.job4j.dreamjob.configuration.DatasourceConfiguration;
+import ru.job4j.dreamjob.model.User;
+
+import java.util.List;
+import java.util.Properties;
+
+import static java.util.Collections.emptyList;
+import static java.util.Optional.empty;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class Sql2oUserRepositoryTest {
+
+    private static Sql2oUserRepository sql2oUserRepository;
+
+    private static Sql2o sql2o;
+
+    @BeforeAll
+    public static void initRepositories() throws Exception {
+        var properties = new Properties();
+        try (var inputStream = Sql2oUserRepository.class.getClassLoader().getResourceAsStream("connection.properties")) {
+            properties.load(inputStream);
+        }
+        var url = properties.getProperty("datasource.url");
+        var username = properties.getProperty("datasource.username");
+        var password = properties.getProperty("datasource.password");
+
+        var configuration = new DatasourceConfiguration();
+        var datasource = configuration.connectionPool(url, username, password);
+        sql2o = configuration.databaseClient(datasource);
+
+        sql2oUserRepository = new Sql2oUserRepository(sql2o);
+    }
+
+    @AfterEach
+    private void clearUsersTable() {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("DELETE FROM users;");
+            query.executeUpdate();
+        }
+    }
+
+    @Test
+    public void whenSaveThenGetSame() {
+        var user = sql2oUserRepository.save(new User(0, "email", "name", "password")).get();
+        var savedUser = sql2oUserRepository.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        assertThat(savedUser.get()).usingRecursiveComparison().isEqualTo(user);
+    }
+
+    @Test
+    public void whenSaveSeveralThenGetAll() {
+        var user1 = sql2oUserRepository.save(new User(0, "email1", "name1", "password1")).get();
+        var user2 = sql2oUserRepository.save(new User(0, "email2", "name2", "password2")).get();
+        var user3 = sql2oUserRepository.save(new User(0, "email3", "name3", "password3")).get();
+        var result = sql2oUserRepository.findAll();
+        assertThat(result).isEqualTo(List.of(user1, user2, user3));
+    }
+
+    @Test
+    public void whenDontSaveThenNothingFound() {
+        assertThat(sql2oUserRepository.findAll()).isEqualTo(emptyList());
+        assertThat(sql2oUserRepository.findByEmailAndPassword("email", "password")).isEqualTo(empty());
+    }
+
+    @Test
+    public void whenSameEmailAdd() {
+        var user1 = sql2oUserRepository.save(new User(0, "email", "name1", "password1"));
+        var user2 = new User(0, "email", "name2", "password2");
+        assertThatThrownBy(() -> sql2oUserRepository.save(user2))
+                .isInstanceOf(Sql2oException.class);
+    }
+
+}
